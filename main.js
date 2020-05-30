69187977a6dc496a2fb1e56ad0e9f1b639680c91
Daniel Marimbo <dmarimbo24@gmail.com>
2020-05-30 14:46:23 +0300

    minor change

--- main.js
+++ main.js
@@ -11,34 +11,46 @@ if (computerPlay < .33){
     computerPlay = 'scissor';
 }
 
-console.log(computerPlay);
+// console.log(computerPlay);
+
+// function playRound(playerSelection, computerSelection){
+//     if(playerSelection === computerSelection) {
+//         return "Game was a tie"
+//     } else if(playerSelection === "rock"){
+//         if(computerSelection === "paper"){
+//             return "you lose! paper beats rock"
+//         } else if(computerSelection === "scissor"){
+//             return "You win! rock beats scissor"
+//         }
+//     } 
+//     else if(playerSelection==="paper"){
+//         if(computerSelection==="rock"){
+//             return "you win! paper beats rock"
+//         } else if (computerSelection==="scissor"){
+//             return "you lose! scissor beats paper"
+//         }
+//     }
+//     else if (playerSelection==="scissor"){
+//         if(computerSelection==="rock"){
+//             return "you lose! rock beats scissor"
+//         } else if(computerSelection==="paper"){
+//             return "you win! scissor beats paper"
+//         }
+//     } else{
+//         return "please enter the right choice"
+//     }
+// }
+
+// console.log(playRound(userPlay, computerPlay));
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection) {
-        return "Game was a tie"
-    } else if(playerSelection === "rock"){
-        if(computerSelection === "paper"){
-            return "you lose! paper beats rock"
-        } else if(computerSelection === "scissor"){
-            return "You win! rock beats scissor"
-        }
-    } 
-    else if(playerSelection==="paper"){
-        if(computerSelection==="rock"){
-            return "you win! paper beats rock"
-        } else if (computerSelection==="scissor"){
-            return "you lose! scissor beats paper"
-        }
-    }
-    else if (playerSelection==="scissor"){
-        if(computerSelection==="rock"){
-            return "you lose! rock beats scissor"
-        } else if(computerSelection==="paper"){
-            return "you win! scissor beats paper"
-        }
-    } else{
-        return "please enter the right choice"
+    if(playerSelection === computerSelection){
+        return "its a tie"
+    } else if((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection==="paper" && computerSelection==="rock") || (playerSelection==="scissor" && computerSelection==="paper")){
+        return "You win!"
+    } else {
+        return "You lose!"
     }
 }
 
-console.log(playRound(userPlay, computerPlay));
+// console.log(playRound(userPlay, computerPlay));
