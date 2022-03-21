8bf99d5ddb2d0ae98772b657294a9b901a6cc446
Daniel Marimbo <dmarimbo24@gmail.com>
2022-03-22 00:11:31 +0300

    Make some corrections

--- main.js
+++ main.js
@@ -1,56 +1,9 @@
-var userPlay = prompt("rock, paper or scissor?");
-document.write(userPlay);
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = getElementById("userScore");
+const computerScore_span = getElementById("computerScore");
+const result = getElementById("result");
+const rockChoice = getElementById("r");
+const paperChoice = getElementById("p");
+const scissorChoice = getElementById("s");
 
-var computerPlay = Math.random();
-
-if (computerPlay < .33){
-    computerPlay = 'rock';
-} else if (computerPlay <= .66) {
-    computerPlay = 'paper';
-} else {
-    computerPlay = 'scissor';
-}
-
-// console.log(computerPlay);
-
-// function playRound(playerSelection, computerSelection){
-//     if(playerSelection === computerSelection) {
-//         return "Game was a tie"
-//     } else if(playerSelection === "rock"){
-//         if(computerSelection === "paper"){
-//             return "you lose! paper beats rock"
-//         } else if(computerSelection === "scissor"){
-//             return "You win! rock beats scissor"
-//         }
-//     } 
-//     else if(playerSelection==="paper"){
-//         if(computerSelection==="rock"){
-//             return "you win! paper beats rock"
-//         } else if (computerSelection==="scissor"){
-//             return "you lose! scissor beats paper"
-//         }
-//     }
-//     else if (playerSelection==="scissor"){
-//         if(computerSelection==="rock"){
-//             return "you lose! rock beats scissor"
-//         } else if(computerSelection==="paper"){
-//             return "you win! scissor beats paper"
-//         }
-//     } else{
-//         return "please enter the right choice"
-//     }
-// }
-
-// console.log(playRound(userPlay, computerPlay));
-
-function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        return "its a tie"
-    } else if((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection==="paper" && computerSelection==="rock") || (playerSelection==="scissor" && computerSelection==="paper")){
-        return "You win!"
-    } else {
-        return "You lose!"
-    }
-}
-
-// console.log(playRound(userPlay, computerPlay));
